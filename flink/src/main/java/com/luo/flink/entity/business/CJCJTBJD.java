f1713138f39d09030112412cc759935b3fa64b4b
luo-Xiao-Mo <63275895+luo-Xiao-Mo@users.noreply.github.com>
2022-07-05 10:22:15 +0800

    新增oracle表类

---
+++ flink/src/main/java/com/luo/flink/entity/business/CJCJTBJD.java
@@ -0,0 +1,10 @@
+package com.luo.flink.entity.business;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class CJCJTBJD {
+}
---
+++ flink/src/main/java/com/luo/flink/entity/business/GYMLTJZY.java
@@ -0,0 +1,13 @@
+package com.luo.flink.entity.business;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class GYMLTJZY {
+
+    private String code;
+    private String name;
+}
