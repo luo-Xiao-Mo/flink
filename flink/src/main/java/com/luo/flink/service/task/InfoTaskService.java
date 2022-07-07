aa89b1839b8ae44cf6833cf84d40da8f96f88f86
luo-Xiao-Mo <63275895+luo-Xiao-Mo@users.noreply.github.com>
2022-07-07 16:12:14 +0800

    提交测试demo

--- flink/src/main/java/com/luo/flink/service/task/InfoTaskService.java
+++ flink/src/main/java/com/luo/flink/service/task/InfoTaskService.java
@@ -1,5 +1,6 @@
 package com.luo.flink.service.task;
 
+import com.alibaba.fastjson.JSON;
 import com.luo.flink.entity.business.Info;
 import com.luo.flink.service.dao.AbstractService;
 import com.luo.flink.service.dao.InfoService;
@@ -16,26 +17,50 @@ public class InfoTaskService extends AbstractTaskService {
                     .SID(String.valueOf(item.getField("SID")))
                     .SNAME(String.valueOf(item.getField("SNAME")))
                     .build();
-            abstractService.invoke(info);
+//            abstractService.invoke(info);
+            System.out.println(JSON.toJSONString(info));
         });
     }
+
+//    @Override
+//    String getSql() {
+//        return "CREATE TABLE info (\n" +
+//                "     SID INT,\n" +
+//                "     SNAME STRING,\n" +
+//                "     SEX STRING,\n" +
+//                "     PRIMARY KEY(SID) NOT ENFORCED\n" +
+//                "     ) WITH (\n" +
+//                "     'connector' = 'oracle-cdc',\n" +
+//                "     'hostname' = 'localhost',\n" +
+//                "     'port' = '1521',\n" +
+//                "     'username' = 'family',\n" +
+//                "     'password' = 'zyhcdc',\n" +
+//                "     'database-name' = 'XE',\n" +
+//                "     'schema-name' = 'FAMILY',\n" +
+//                "     'table-name' = 'INFO',\n" +
+//                "     'debezium.log.mining.continuous.mine'='true',\n" +
+//                "     'debezium.log.mining.strategy'='online_catalog',\n" +
+//                "     'debezium.database.tablename.case.insensitive'='false',\n" +
+//                "     'scan.startup.mode' = 'initial')";
+//    }
+
 
     @Override
     String getSql() {
         return "CREATE TABLE info (\n" +
-                "     SID INT,\n" +
+                "     SID STRING,\n" +
                 "     SNAME STRING,\n" +
                 "     SEX STRING,\n" +
                 "     PRIMARY KEY(SID) NOT ENFORCED\n" +
                 "     ) WITH (\n" +
                 "     'connector' = 'oracle-cdc',\n" +
-                "     'hostname' = 'localhost',\n" +
+                "     'hostname' = '172.10.120.86',\n" +
                 "     'port' = '1521',\n" +
-                "     'username' = 'family',\n" +
-                "     'password' = 'zyhcdc',\n" +
-                "     'database-name' = 'XE',\n" +
-                "     'schema-name' = 'FAMILY',\n" +
-                "     'table-name' = 'INFO',\n" +
+                "     'username' = 'TEST_SHANGHAIJCPT_DB_FLINK',\n" +
+                "     'password' = 'Abc123!@Gwssi',\n" +
+                "     'database-name' = 'ora12c',\n" +
+                "     'schema-name' = 'TEST_SHANGHAIJCPT_DB_FLINK',\n" +
+                "     'table-name' = 'MESSAGE',\n" +
                 "     'debezium.log.mining.continuous.mine'='true',\n" +
                 "     'debezium.log.mining.strategy'='online_catalog',\n" +
                 "     'debezium.database.tablename.case.insensitive'='false',\n" +
